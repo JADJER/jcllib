7970924f92fbe8224397560e416584a6e99a303d
jadjer <jadjer@ya.ru>
2022-03-29 00:53:54 +0300

    feat: Added catch2 test framework

---
+++ test/main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by jadjer on 29.03.2022.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
---
+++ test/node_test.cpp
@@ -0,0 +1,27 @@
+//
+// Created by jadjer on 29.03.2022.
+//
+
+#include <catch2/catch.hpp>
+
+SCENARIO( "vector can be sized and resized" ) {
+  GIVEN( "An empty vector" ) {
+    auto v = std::vector<std::string>{};
+
+    // Validate assumption of the GIVEN clause
+    THEN( "The size and capacity start at 0" ) {
+      REQUIRE( v.size() == 0 );
+      REQUIRE( v.capacity() == 0 );
+    }
+
+    // Validate one use case for the GIVEN object
+    WHEN( "push_back() is called" ) {
+      v.push_back("hullo");
+
+      THEN( "The size changes" ) {
+        REQUIRE( v.size() == 1 );
+        REQUIRE( v.capacity() >= 1 );
+      }
+    }
+  }
+}

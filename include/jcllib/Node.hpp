deb683698c861b78ef72415ea32738283961f5e3
jadjer <jadjer@ya.ru>
2022-03-28 18:42:05 +0300

    Initial commit

---
+++ include/jcllib/Node.hpp
@@ -0,0 +1,13 @@
+//
+// Created by jadjer on 28.03.2022.
+//
+
+#ifndef JCLLIB_INCLUDE_JCLLIB_NODE_HPP_
+#define JCLLIB_INCLUDE_JCLLIB_NODE_HPP_
+
+#include <dbus/dbus.h>
+
+class Node {
+};
+
+#endif//JCLLIB_INCLUDE_JCLLIB_NODE_HPP_
---
+++ src/Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jadjer on 28.03.2022.
+//
+
+#include <jcllib/Node.hpp>
